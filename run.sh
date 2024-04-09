905199662148f1587551c4ce398655497f9501f8
Thieme <46195238+ThiemeH@users.noreply.github.com>
2024-04-09 22:47:34 +0200

    Hide more services in run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker compose build && docker compose up --no-attach rabbitmq --no-attach mongo-database
+docker compose build && docker compose up --no-attach rabbitmq --no-attach mongo-database --no-attach grafana --no-attach prometheus --no-attach mongodb-exporter
