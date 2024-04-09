c22cd16ad78b279a60d3be0efd16ad139cff1db7
Thieme <46195238+ThiemeH@users.noreply.github.com>
2024-04-09 22:26:48 +0200

    Added crypto import

--- appgateway-service/src/index.ts
+++ appgateway-service/src/index.ts
@@ -1,3 +1,4 @@
+import crypto from 'crypto'
 import express from 'express'
 import promMid from 'express-prometheus-middleware'
 import mongoose from 'mongoose'
@@ -27,7 +28,6 @@ async function run() {
 		)
 
 	const app = express()
-
 
 	app.get('/orders', async (req, res) => {
 		const orders = await Order.find()
@@ -83,13 +83,15 @@ async function run() {
 
 	const PORT = 3005
 
-	app.use(promMid({
-		metricsPath: '/metrics',
-		collectDefaultMetrics: true,
-		requestDurationBuckets: [0.1, 0.5, 1, 1.5],
-		requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
-		responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400]
-	}))
+	app.use(
+		promMid({
+			metricsPath: '/metrics',
+			collectDefaultMetrics: true,
+			requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+			requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+			responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+		})
+	)
 
 	app.listen(PORT, () => {
 		console.log(`Server is running on port ${PORT}`)
@@ -97,5 +99,4 @@ async function run() {
 }
 
 run()
-
 
